aa7bf1ba8045278a093e3f75318668da1896a881
David Macdonald <ardothe1@yahoo.com>
2019-03-21 15:34:05 -0500

    feat(user): user api routes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/user.routes');
+
+const { isLoggedin } = require('./middlewares/auth.middleware');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/users', isLoggedin, userRoutes);
 
 module.exports = router;
---
+++ api/routes/user.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const userCtrl = require('../controllers/user.controller');
+const { isAdmin } = require('../middlewares/auth.middleware');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(isAdmin, userCtrl.create)
+  .get(userCtrl.list);
+
+router
+  .route('/:id')
+  .get(userCtrl.read)
+  .put(isAdmin, userCtrl.update)
+  .delete(isAdmin, userCtrl.remove);
+
+router.param('id', userCtrl.getByIdMiddleware);
+
+module.exports = router;
