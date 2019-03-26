17ee7a4d76f4bb04b8e30175a4b9eafa615eaba9
David Macdonald <ardothe1@yahoo.com>
2019-03-26 09:44:04 -0500

    feat(auth): auth middleware for group access

--- api/middlewares/auth.middleware.js
+++ api/middlewares/auth.middleware.js
@@ -1,24 +1,59 @@
 const passport = require('passport');
 const APIError = require('../utils/api-error');
 const ROLES = require('../constants/roles');
+const GROUP_PERMISSION = require('../constants/group-permission');
 
 const isLoggedin = passport.authenticate('jwt', { session: false });
 
-// @TODO check group access
+// role based
 const hasRole = role => (req, res, next) => {
-  if ((req.user && req.user.role === role) || req.user.role === 'ADMIN') {
+  if ((req.user && req.user.role === role) || req.user.role === ROLES.ADMIN) {
     next();
   } else {
-    throw new APIError('You are forbidden to access this resource', 403);
+    next(new APIError('You are forbidden to access this resource', 403));
   }
 };
 
 const isAdmin = hasRole(ROLES.ADMIN);
 const isUser = hasRole(ROLES.USER);
 
+// group role based
+const hasAccess = permission => (req, res, next) => {
+  if (req.user.role === ROLES.ADMIN) {
+    return next();
+  }
+
+  const perm = req.user.groups.find(g => g.group === req.group._id.toString());
+
+  if (
+    (perm && perm.permission === permission) ||
+    perm.permission === GROUP_PERMISSION.ADMIN
+  ) {
+    return next();
+  }
+
+  return next(new APIError('You are forbidden to access this resource', 403));
+};
+
+const isGroupAdmin = hasAccess(GROUP_PERMISSION.ADMIN);
+const isGroupMember = hasAccess(GROUP_PERMISSION.MEMBER);
+
+// owner based
+const isMe = (req, res, next) => {
+  if (req.user._id === req.queryUser._id || req.user.role === ROLES.ADMIN) {
+    return next();
+  }
+
+  return next(new APIError('You are forbidden to access this resource', 403));
+};
+
 module.exports = {
   isLoggedin,
   isAdmin,
   isUser,
-  hasRole
+  isMe,
+  hasRole,
+  hasAccess,
+  isGroupAdmin,
+  isGroupMember
 };
