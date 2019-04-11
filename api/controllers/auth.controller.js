64da1e8279abbd6ea85afa106a6848b8f3dda060
David Macdonald <ardothe1@yahoo.com>
2019-04-11 10:23:24 -0500

    fix(auth): avoid deleted groups

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,6 +70,7 @@ function login(req, res, next) {
             p =>
               (p.permission === GROUP_PERMISSION.MEMBER ||
                 p.permission === GROUP_PERMISSION.ADMIN) &&
+              p.group &&
               p.group.chat
           )
           .map(p => p.group.chat.toString());
