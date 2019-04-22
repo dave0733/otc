a12606d5cf59059ab17b8a071b69a10a353f3c97
David Macdonald <ardothe1@yahoo.com>
2019-04-22 04:06:29 -0500

    feat(mailer): send mail through cloud functions

---
+++ api/constants/mail-type.js
@@ -0,0 +1,15 @@
+module.exports = {
+  WELCOME: 'welcome',
+  VERIFICATION: 'verification',
+  RESET_PASSWORD: 'resetPassword',
+  APPLICATION_RECEIVED: 'applicationReceived',
+  GROUP_REQUEST: 'groupRequest',
+  VOUCH_DENIED: 'vouchDenied',
+  VOUCH_ACCEPTED: 'vouchAccepted',
+  VOUCH_REQUEST_RECEIVED: 'vouchRequestReceived',
+  PROPOSAL_ACCEPTED: 'proposalAccepted',
+  PROPOSAL_REJECTED: 'proposalRejected',
+  OFFER_CREATED: 'newOffer',
+  OFFER_EXPIRED: 'offerExpired',
+  OFFER_ENDED: 'offerEnded'
+};
