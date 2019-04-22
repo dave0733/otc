8c81b426c5d10473bb70d5f463d19bb53a4128e6
David Macdonald <ardothe1@yahoo.com>
2019-04-22 04:06:04 -0500

    feat(cloud-functions): mailer and recent messages function

---
+++ functions/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  sendgridApiKey:
+    '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',
+  fromMailAddress: 'no-reply@otctrade.com',
+  fromName: 'OTCTrade',
+  mailTemplates: {
+    resetPassword: 'd-e8207105350e48b6b8fa0e265033d9bc',
+    welcome: 'd-0e1cf5555c674f2c867a74fecba28f3c',
+    verification: 'd-720b9ce951bb4eabae30fd8faff3fcf6',
+    applicationReceived: 'd-1150271e732f4b489fe98b34cf2a9588',
+    groupRequest: 'd-0a1478c1e959455a942eef74297562d2',
+    vouchDenied: 'd-c77ab4c3006f4336a6c15b518a969d76',
+    vouchAccepted: 'd-48bc3976380643e8bf2e1ca14642e2d1',
+    newOffer: 'd-b87fab064d6f48adad051da2990b73a4',
+    offerExpired: 'd-c4c0971ccf4f49ab89ea7850a5f748f6',
+    offerEnded: 'd-11d608549db244ceb7bf459088299ab9',
+    proposalAccepted: 'd-f98a9fb848c943f7a68127a6b43b9dba',
+    proposalRejected: 'd-e8aa4597630347aebda2497edda09d9b'
+  }
+};
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const sendMail = require('./mailer');
+const handleRecentMessage = require('./recent-message');
 
 admin.initializeApp();
 
-exports.hello = functions.https.onRequest((req, res) => {
-  res.json({ message: 'hello world' });
-});
+exports.mailer = functions.firestore
+  .document(`mails/{mailID}`)
+  .onCreate((snap, context) => {
+    const data = snap.data();
+    const ref = snap.ref;
+    // const { taskID } = context.params;
+    return sendMail(ref, data);
+  });
+
+exports.recentMessage = functions.firestore
+  .document('chats/{chatID}/messages/{messageID}')
+  .onCreate((snap, context) => {
+    const data = snap.data();
+    const { chatID, messageID } = context.params;
+
+    return handleRecentMessage(chatID, messageID, data);
+  });
